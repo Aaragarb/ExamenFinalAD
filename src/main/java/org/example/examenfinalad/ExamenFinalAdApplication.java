ceba9c6976bae4971550f7df6af14b8fea6add2d
Aaragarb <alejandroaragongarcia03@gmail.com>
2025-03-03 09:44:06 +0100

    Inicio

---
+++ src/main/java/org/example/examenfinalad/ExamenFinalAdApplication.java
@@ -0,0 +1,13 @@
+package org.example.examenfinalad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenFinalAdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenFinalAdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/examenfinalad/ExamenFinalAdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.examenfinalad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenFinalAdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
